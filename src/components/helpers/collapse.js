5839aa3a74a9399ad4b515a10ead830dfb853d4b
Jody Duke <contact@jodyduke.co.uk>
2018-03-15 16:20:18 +0000

    added collapse component for history sidebar

---
+++ src/components/helpers/collapse.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Collapse extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHidden : props.hidden
+        }
+        console.log(props.hidden)
+    }
+
+    toggleHidden(){
+        this.setState({
+            isHidden : !this.state.isHidden
+        })
+    }
+
+
+    render(){
+        let title = this.state.isHidden ? this.props.name + ' ►' : this.props.name + ' ▼'
+        return(
+            <div>
+                <p className="collapse-header" onClick={this.toggleHidden.bind(this)}>{title}</p>
+                {!this.state.isHidden && this.props.content}
+            </div>
+        )
+    }
+}
+
+export default Collapse
