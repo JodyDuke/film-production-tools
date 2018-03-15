9a98a589c966f77046dd9248c725c6c73ec96f5e
Jody Duke <contact@jodyduke.co.uk>
2018-03-15 16:20:58 +0000

    added beta tag

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -9,12 +9,15 @@ class Menu extends Component {
                 <div className="logo">
                     <i className="fa fa-film fa-5x"></i>
                 </div>
-                <h1>Film Production<br/>Tools</h1>
+                <div>
+                    <h1>Film Production<br/>Tools</h1>
+                    <h4>beta v2.0</h4>
+                </div>
                     <NavItem nav="/" linkName="Home" faIcon="fa fa-home fa-3x" className="navLink blue" />
                     <NavItem nav="/holiday-pay-calculator" linkName="Holiday Pay Calculator" faIcon="fa fa-plane fa-3x" className="navLink green" />
                     <NavItem nav="/date-difference-calculator" linkName="Date Difference Calculator" faIcon="fa fa-calendar-o fa-3x" className="navLink yellow"/>
                     <NavItem nav="/framerate-calculator" linkName="Framerate Calculator" faIcon="fa fa-film fa-3x" className="navLink red"/>
-                    <NavItem nav="/time-calculator" linkName="Time Calculator" faIcon="fa fa-clock-o fa-3x" className="navLink orange"/>
+                    <NavItem nav="/time-calculator" linkName="Time Calculator" faIcon="fa fa-clock-o fa-3x" className="navLink purple"/>
 
             </div>
         )
