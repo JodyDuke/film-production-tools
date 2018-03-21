c16f43b0d6744833490787310ab8c40d00aced93
Jody Duke <contact@jodyduke.co.uk>
2018-03-21 16:50:02 +0000

    added icons to component and styled forms

--- src/components/main/form.js
+++ src/components/main/form.js
@@ -4,8 +4,9 @@ class FormItem extends Component {
     render() {
         return(
             <label className={this.props.className}>
+                <i className={this.props.faIcon} />
+                <input name={this.props.name} type={this.props.type} value={this.props.value} placeholder={this.props.placeHolder} onChange={this.props.onChange} />
                 {this.props.text}
-                <input name={this.props.name} type={this.props.type} value={this.props.value} onChange={this.props.onChange} />
             </label>
         )
     }
