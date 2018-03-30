ba46984ebc9ea515448404eae3fb735aa732e420
Jody Duke <contact@jodyduke.co.uk>
2018-03-30 16:54:21 +0100

    added support for decimals in forms

--- src/components/main/form.js
+++ src/components/main/form.js
@@ -5,7 +5,7 @@ class FormItem extends Component {
         return(
             <label className={this.props.className}>
                 <i className={this.props.faIcon} />
-                <input name={this.props.name} type={this.props.type} value={this.props.value} placeholder={this.props.placeHolder} onChange={this.props.onChange} />
+                <input name={this.props.name} type={this.props.type} value={this.props.value} placeholder={this.props.placeHolder} step="any" onChange={this.props.onChange} />
                 {this.props.text}
             </label>
         )
