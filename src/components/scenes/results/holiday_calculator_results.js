84706e9ba324f4bc1d07dfc0431cd5f7b9022336
Jody Duke <contact@jodyduke.co.uk>
2018-03-30 16:55:52 +0100

    formatted results

--- src/components/scenes/results/holiday_calculator_results.js
+++ src/components/scenes/results/holiday_calculator_results.js
@@ -5,12 +5,20 @@ class HolidayResult extends Component {
         return (
             <div className="holiday-results">
                 <div className="data">
-                    <h3>Total pay: £{this.props.data[0]}</h3>
-                    <h3>Total holiday owed: {this.props.data[1]} days</h3>
-                    <h3>Holiday remaining: {this.props.data[2]} days</h3>
+                    <h2>£{this.props.data[0]}</h2>
+                    <h3>Total pay</h3>
+                </div>
+                <div className="data">
+                    <h2>{this.props.data[1]} days</h2>                    
+                    <h3>Total holiday owed</h3>
+                </div>
+                <div className="data">
+                    <h2>{this.props.data[2]} days</h2>
+                    <h3>Holiday remaining</h3>
                 </div>
                 <div className="result">
-                    <h3>Total owed: £{this.props.result}</h3>
+                    <h2>£{this.props.result}</h2>
+                    <h3>Total owed</h3>
                 </div>
             </div>
         )
