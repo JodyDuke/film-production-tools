a7a4dbddc99a6f5631bf206911b9b2f371f10689
Jody Duke <contact@jodyduke.co.uk>
2018-04-09 15:49:13 +0100

    added intro

--- src/components/scenes/home.js
+++ src/components/scenes/home.js
@@ -3,7 +3,14 @@ import React, { Component } from "react";
 class Home extends Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <div className="intro">
+                    <p>Welcome to the beta of Film Production Tools. A selection of tools built to make some of your everyday film production tasks easier. 
+                        The tools can be found in the menu bar on the left hand side, a history of your calculations can be found by clicking on the history button
+                        in the top right hand corner of the screen.
+                    </p>
+                </div>
+            </div>
         )
     }
 
