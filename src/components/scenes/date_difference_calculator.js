ab93e40dfd7ff9a5959b2f36bfa22d14b50db43f
Jody Duke <contact@jodyduke.co.uk>
2018-04-09 13:02:41 +0100

    added submit and clear handlers

--- src/components/scenes/date_difference_calculator.js
+++ src/components/scenes/date_difference_calculator.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import FormItem from '../main/form.js';
 import { dateDiff } from '../logic/date_difference_logic';
+import DateResult from './results/date_calculator_results';
 
 class DateDifferenceCalculator extends Component {
     constructor(props) {
@@ -12,7 +13,25 @@ class DateDifferenceCalculator extends Component {
             total: ''
         }
 
-        this.handleChange = this.handleChange.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.clear = this.clear.bind(this)
+    }
+
+    clear() {
+        this.setState({
+            start : '',
+            end : '',
+            total : ''
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let result = dateDiff(this.state.start, this.state.end)
+        this.setState({
+            total : result
+        })
     }
 
     handleChange(e) {
@@ -28,13 +47,18 @@ class DateDifferenceCalculator extends Component {
                         <button className="clear" onClick={this.clear}>Clear</button>
                     </div>
                 </div>
+                <div className="infoBox">
+                    <p>blah blah blah</p>
+                </div>
                 <div className="date-input">
                     <form onSubmit={this.handleSubmit}>
-                        <FormItem className="form-element" type="date" name="start" text="start" placeHolder='0' value={this.state.start} onChange={this.handleChange.bind(this)} />
-                        <FormItem className="form-element" type="date" name="end" text="end" placeHolder='0' value={this.state.end} onChange={this.handleChange.bind(this)} />
-                        <input type="submit" value="submit" />
+                        <FormItem className="form-element" type="date" name="start" text="start" value={this.state.start} onChange={this.handleChange.bind(this)} />
+                        <FormItem className="form-element" type="date" name="end" text="end" value={this.state.end} onChange={this.handleChange.bind(this)} />
+                        <input type="submit" value="Calculate" />
                     </form>
                 </div>
+
+                {this.state.total ? <DateResult total={this.state.total} /> : ''}
             </div>
         )
     }
